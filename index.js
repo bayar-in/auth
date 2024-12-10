3ccac7f18ea7378d55ce8a9f1c1bde0b38536d39
MUHAMMAD RIFKY <muhammad26rifky06@gmail.com>
2024-12-10 19:21:52 +0700

    fix: update redirect path to use absolute URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ getJSON("https://asia-southeast2-awangga.cloudfunctions.net/bayarin/data/user","
 function responseFunction(result){
     if (result.status === 200){
         setInner("content","Selamat datang "+result.data.name);
-        redirect("src/page/dashboard/dashboard.html");
+        redirect("/src/page/dashboard/dashboard.html");
     }else{
         getJSON("https://asia-southeast2-awangga.cloudfunctions.net/bayarin/data/konsumen/user","login",getCookie("login"),apiResponse)
     }
